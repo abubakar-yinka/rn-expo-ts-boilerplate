c09b50d606d2c83286f5162177f9a0f497f8c350
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-24 03:56:57 +0100

    refactor: I18n setup modified to conform with latest version

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,17 +1,17 @@
 import * as Localization from "expo-localization";
-import i18n from "i18n-js";
+import { I18n } from "i18n-js";
 // Languages
 import English from "./lang/en";
 
 // Set the key-value pairs for the different languages to support.
-i18n.translations = {
+const i18n = new I18n({
   en: English,
-};
+});
 
 // Set the locale once at the beginning of the app.
-i18n.locale = Localization.locale;
+i18n.locale = Localization.locale || document.documentElement.lang || "en";
 
 // When a value is missing from a language it'll fallback to another language with the key present.
-i18n.fallbacks = true;
+i18n.enableFallback = true;
 
 export default i18n;
