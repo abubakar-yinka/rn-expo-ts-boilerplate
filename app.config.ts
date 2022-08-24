fae2e4c2d42966c5aa5c9f724aeb402e7be8f327
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-24 03:52:02 +0100

    chore: app config setup

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+import { ExpoConfig } from "@expo/config-types";
+
+const config: ExpoConfig = {
+  owner: "abubakar_y_i",
+  name: "boilerplate-native",
+  slug: "boilerplate-native",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+};
+
+export default config;
